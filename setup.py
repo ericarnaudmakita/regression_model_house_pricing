2c099ff1cf2360a09b7bcdfc4b510c3a38c28588
Eric Makita <eric.makita-extern@renault.com>
2021-09-03 23:57:42 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is about predicting the price of a house based on its features',
+    author='Eric Makita',
+    license='MIT',
+)
